cfbb20be66f8f5819640ab10261d6e255cad0784
Ach98 <74909623+Ach98@users.noreply.github.com>
2026-04-11 00:14:12 +0200

    just a test

--- inc/SafetyManager.h
+++ inc/SafetyManager.h
@@ -8,7 +8,6 @@
 #include "../inc/Constants.h"     // For NUM_CELLS and limits
 
 
-
 /**
  * @brief Manages the safety state of the BMS based on battery cell parameters and pack current.
  * This class evaluates cell voltages, temperatures, and pack current against defined limits
